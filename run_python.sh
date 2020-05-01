08a1876df0a7ce7aa725dde25f280c21b76807fb
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-01 21:52:07 +0530

    Add Run Python script
    run_python.sh

---
+++ run_python.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function Update() {
+  git fetch origin
+  git pull origin master
+}
+
+echo "Ready?"
+echo "Enter '1' for yes and anything else to Exit"
+read -r temp
+if [[ ${temp} = "1" ]]; then
+  cd "python" || exit
+  echo "Pulling Programs from my GitHub"
+  echo ""
+  Update
+  echo ""
+  sleep 3
+  bash build.sh
+else
+    echo ""
+    echo "Uh-Huh! Wrong Input"
+    echo "Exiting!"
+    sleep 2
+    clear
+    exit
+fi
