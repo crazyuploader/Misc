df6033b29ea68c107c4d8598325521353059bc67
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-05-27 12:02:01 +0530

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -8,7 +8,7 @@ Write-Output ""
 Write-Output ""
 Write-Output "Going to ping the following servers -"
 Write-Output ""
-$Servers = "1.1.1.1", "8.8.8.8", "75.2.66.166", "128.1.120.194", "google.com", "quicksight.ap-south-1.amazonaws.com", "quicksight.ap-southeast-1.amazonaws.com", "mumbaispeedtest1.airtel.in", "speedtestggn1.airtel.in", "speedtestchn1.airtel.in"
+$Servers = "1.1.1.1", "8.8.8.8", "75.2.66.166", "128.1.120.194", "google.com", "quicksight.ap-south-1.amazonaws.com", "quicksight.ap-southeast-1.amazonaws.com", "mumbaispeedtest1.airtel.in", "speedtestdel.airtelbroadband.in", "speedtestchn1.airtel.in"
 foreach ($Server in $Servers) {
     Write-Output $Server
 }
